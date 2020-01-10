559dd5c931b624b93113227ad82bb87f00e844e6
Denis Yakshov <feerzlay@gmail.com>
2020-01-10 15:06:36 +0300

    Setup stylelint

--- projects/elonkit/src/lib/elonkit.component.ts
+++ projects/elonkit/src/lib/elonkit.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
 
 @Component({
   selector: 'es-elonkit',
-  template: `
-    <p>
-      elonkit works!
-    </p>
-    <p>
-      {{ text }}
-    </p>
-    <button (click)="onClick()">Emit "Hello"</button>
-  `,
-  styles: []
+  templateUrl: './elonkit.component.html',
+  styleUrls: ['./elonkit.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
-export class ElonkitComponent implements OnInit {
+export class ElonkitComponent {
   @Input() text: string;
 
   @Output() hello = new EventEmitter<string>();
 
   constructor() {}
-
-  ngOnInit() {}
 
   public onClick() {
     this.hello.emit('Hello');
