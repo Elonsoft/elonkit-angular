f49fbff98fabcf8cdfd0ed19c227fee83765847e
Denis Yakshov <feerzlay@gmail.com>
2020-01-17 18:33:17 +0300

    Add inline-form-field component

---
+++ projects/elonkit/src/lib/ui/inline-form-field/__stories__/inline-form-field-story-basic/index.ts
@@ -0,0 +1,2 @@
+export { InlineFormFieldStoryBasicModule } from './inline-form-field-story-basic.module';
+export { InlineFormFieldStoryBasicComponent } from './inline-form-field-story-basic.component';
---
+++ projects/elonkit/src/lib/ui/inline-form-field/index.ts
@@ -0,0 +1,7 @@
+export { ESInlineFormFieldModule } from './inline-form-field.module';
+export { InlineFormFieldComponent as ESInlineFormFieldComponent } from './inline-form-field.component';
+export {
+  InlineFormFieldLocale,
+  InlineFormFieldLocaleEN,
+  InlineFormFieldLocaleRU
+} from './inline-form-field.component.locale';
