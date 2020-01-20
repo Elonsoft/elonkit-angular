bd3dbcc76c8b6bce9c7b95179503168150dcb28b
Sorkin Vladislav <v.sorkin@elonsoft.ru>
2020-01-20 11:01:23 +0300

    Add heading component

---
+++ projects/elonkit/src/lib/ui/heading/heading.component.ts
@@ -0,0 +1,20 @@
+import { Component, ChangeDetectionStrategy, ViewEncapsulation, Input } from '@angular/core';
+
+@Component({
+  selector: 'es-heading',
+  templateUrl: './heading.components.html',
+  styleUrls: ['./heading.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class HeadingComponent {
+  /**
+   * Button title text
+   */
+  @Input() public title = 'Title';
+
+  /**
+   * Route to redirect at click of a button
+   */
+  @Input() public backRoute: string[] = ['..'];
+}
