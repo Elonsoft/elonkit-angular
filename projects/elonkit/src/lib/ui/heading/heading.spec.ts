6a63346d98084831ef1e8c597bf4c41c1f0fd1ca
Sorkin Vladislav <v.sorkin@elonsoft.ru>
2020-01-20 17:56:11 +0300

    Use angular testing library for tests

--- projects/elonkit/src/lib/ui/heading/heading.spec.ts
+++ projects/elonkit/src/lib/ui/heading/heading.spec.ts
@@ -1,59 +1,56 @@
-import { TestBed, async, ComponentFixture, inject, tick, fakeAsync } from '@angular/core/testing';
-import { render } from '@testing-library/angular';
+import { render, RenderResult } from '@testing-library/angular';
 
 import { RouterTestingModule } from '@angular/router/testing';
-import { CommonModule, APP_BASE_HREF, Location } from '@angular/common';
-import { HeadingComponent } from './heading.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { APP_BASE_HREF, Location } from '@angular/common';
+
+import { ESHeadingModule } from './heading.module';
+
+import { inject } from '@angular/core/testing';
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <router-outlet></router-outlet>
+  `
+})
+export class HeadingParentComponent {}
+
+@Component({
+  template: '<es-heading></es-heading>'
+})
+export class HeadingChildComponent {}
 
 const ROUTES = [
   {
     path: '',
-    component: HeadingComponent,
     children: [
       {
-        path: 'some-route',
-        component: HeadingComponent
+        path: 'foo',
+        children: [
+          {
+            path: '',
+            component: HeadingChildComponent
+          },
+          {
+            path: 'bar',
+            component: HeadingChildComponent
+          }
+        ]
       }
     ]
   }
 ];
 
 describe('Heading', () => {
-  let fixture: ComponentFixture<HeadingComponent>;
-  let component: HeadingComponent;
-  let element: HTMLElement;
-  let router: Router;
-  let location: Location;
+  let component: RenderResult<HeadingParentComponent, HeadingParentComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [HeadingComponent],
-      imports: [
-        MatIconModule,
-        MatButtonModule,
-        CommonModule,
-        RouterTestingModule.withRoutes(ROUTES)
-      ],
+  beforeEach(async () => {
+    component = await render(HeadingParentComponent, {
+      imports: [ESHeadingModule, RouterTestingModule.withRoutes(ROUTES)],
+      declarations: [HeadingChildComponent],
       providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    router = TestBed.get(Router);
-    location = TestBed.get(Location);
-
-    fixture = TestBed.createComponent(HeadingComponent);
-    component = fixture.componentInstance;
-    element = fixture.nativeElement;
-
-    component.cdr.detectChanges();
-
-    fixture.ngZone.run(() => {
-      router.navigate(['/some-route']);
     });
+    await component.navigate('/foo/bar');
   });
 
   it('should create the component', () => {
@@ -61,17 +58,21 @@ describe('Heading', () => {
   });
 
   it(`should have as title 'Title'`, () => {
-    expect(component.title).toBe('Title');
+    expect(component.getByText('Title'));
   });
 
   it(`should have as backRoute '..'`, () => {
-    expect(component.backRoute).toEqual(['..']);
+    const element = component.getByLabelText('Back');
+    expect(element.getAttribute('href')).toBe('/foo');
   });
 
-  it(`should change route on link click to '/'`, async () => {
-    expect(location.path()).toBe('/some-route');
-    element.querySelector('a').click();
-    await fixture.whenStable();
+  it(`should change route on link click to '/'`, inject([Location], async (location: Location) => {
+    expect(location.path()).toBe('/foo/bar');
+
+    await component.navigate(component.getByLabelText('Back'));
+    expect(location.path()).toBe('/foo');
+
+    await component.navigate(component.getByLabelText('Back'));
     expect(location.path()).toBe('/');
-  });
+  }));
 });
