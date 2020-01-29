ce00726551c97b9013568226eb130d6b48551504
Denis Yakshov <feerzlay@gmail.com>
2020-01-29 15:20:17 +0300

    Move cancel logic inside of component

--- projects/elonkit/src/ui/inline-form-field/__stories__/inline-form-field-story-cancel/index.ts
+++
@@ -1,3 +0,0 @@
-export { InlineFormFieldStoryCancelModule } from './inline-form-field-story-cancel.module';
-export { InlineFormFieldStoryCancelComponent } from './inline-form-field-story-cancel.component';
-export { INLINE_FORM_FIELD_STORY_CANCEL_SOURCE } from './inline-form-field-story-cancel.source';
