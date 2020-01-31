edbba1edbfc8efa52a08663060bc9f7fef12f18e
Denis Yakshov <feerzlay@gmail.com>
2020-01-31 17:48:04 +0300

    Add documentation for timepicker

--- projects/elonkit/src/ui/timepicker/__stories__/timepicker-story-required/index.ts
+++ projects/elonkit/src/ui/timepicker/__stories__/timepicker-story-required/index.ts
@@ -1,2 +1,3 @@
 export { TimepickerStoryRequiredModule } from './timepicker-story-required.module';
 export { TimepickerStoryRequiredComponent } from './timepicker-story-required.component';
+export { TIMEPICKER_STORY_REQUIRED_SOURCE } from './timepicker-story-required.source';
