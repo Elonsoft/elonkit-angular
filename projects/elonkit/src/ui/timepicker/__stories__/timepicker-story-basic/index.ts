c95a42cc6a7d618d1983e05d9885e9e111959086
Denis Yakshov <feerzlay@gmail.com>
2020-01-31 13:53:30 +0300

    Add timepicker component

---
+++ projects/elonkit/src/ui/timepicker/__stories__/timepicker-story-basic/index.ts
@@ -0,0 +1,2 @@
+export { TimepickerStoryBasicModule } from './timepicker-story-basic.module';
+export { TimepickerStoryBasicComponent } from './timepicker-story-basic.component';
---
+++ projects/elonkit/src/ui/timepicker/__stories__/timepicker-story-seconds/index.ts
@@ -0,0 +1,2 @@
+export { TimepickerStorySecondsModule } from './timepicker-story-seconds.module';
+export { TimepickerStorySecondsComponent } from './timepicker-story-seconds.component';
