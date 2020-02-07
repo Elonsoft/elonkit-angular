e1a77af8aafbb33ea92f698e5c93748848f41009
Denis Yakshov <feerzlay@gmail.com>
2020-02-07 15:01:28 +0300

    @angular/core migration - migration-v9-dynamic-queries Static flag migration. Removes the `static` flag from dynamic queries. As of Angular 9, the "static" flag defaults to false and is no longer required for your view and content queries. Read more about this here: https://v9.angular.io/guide/migration-dynamic-flag

--- projects/elonkit/src/ui/inline-form-field/inline-form-field.component.ts
+++ projects/elonkit/src/ui/inline-form-field/inline-form-field.component.ts
@@ -90,11 +90,11 @@ export class ESInlineFormFieldComponent {
    */
   @Output() cancel = new EventEmitter<ESInlineFormFieldComponent>();
 
-  @ContentChild(MatFormField, { static: false }) private formField: MatFormField;
+  @ContentChild(MatFormField) private formField: MatFormField;
 
-  @ContentChild(NgModel, { static: false }) private ngModel: NgModel;
-  @ContentChild(FormControl, { static: false }) private formControl: FormControl;
-  @ContentChild(FormControlName, { static: false }) private formControlName: FormControlName;
+  @ContentChild(NgModel) private ngModel: NgModel;
+  @ContentChild(FormControl) private formControl: FormControl;
+  @ContentChild(FormControlName) private formControlName: FormControlName;
 
   /**
    * @internal
