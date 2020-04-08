d26d688d1b6b2e66ec2bf841dbeb01c3bca4a485
Denis Yakshov <feerzlay@gmail.com>
2020-04-08 17:46:49 +0300

    [FileUploader] Add support for mat-hint and mat-error

--- projects/elonkit/src/ui/file-uploader/__stories__/file-uploader-story-basic/file-uploader-story-basic.module.ts
+++ projects/elonkit/src/ui/file-uploader/__stories__/file-uploader-story-basic/file-uploader-story-basic.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { FileUploaderStoryBasicComponent } from './file-uploader-story-basic.component';
 
 import { ESFileUploaderModule } from '../..';
 
 @NgModule({
   declarations: [FileUploaderStoryBasicComponent],
-  imports: [CommonModule, ReactiveFormsModule, ESFileUploaderModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, ESFileUploaderModule],
   exports: [FileUploaderStoryBasicComponent]
 })
 export class FileUploaderStoryBasicModule {}
