6a4d46d10ce9130a23f4b83eb70ccff36c98d3e6
Maxim <m.domanov@elonsoft.ru>
2020-06-05 11:33:03 +0300

    Added action heading component, base and customization stories.

--- projects/elonkit/src/public-api.ts
+++ projects/elonkit/src/public-api.ts
@@ -1,3 +1,4 @@
 export * from './ui/counter';
 export * from './ui/inline-form-field';
 export * from './ui/timepicker';
+export * from './ui/action-heading';
---
+++ projects/elonkit/src/ui/action-heading/action-heading.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'es-action-heading',
+  templateUrl: './action-heading.component.html',
+  styleUrls: ['./action-heading.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ESActionHeadingComponent {
+  @Input()
+  public title: string;
+  @Input()
+  public type: 'h1' | 'h2' = 'h1';
+  @Input()
+  public color: 'primary' | 'warn' | 'accent' = 'primary';
+  @Input()
+  public actionPermissions: string | string[];
+  @Output()
+  public action: EventEmitter<any> = new EventEmitter();
+}
