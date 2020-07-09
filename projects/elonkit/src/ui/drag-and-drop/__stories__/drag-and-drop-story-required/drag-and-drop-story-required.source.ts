7f7804a166c39ff6974381407421da74004f0149
Sergei Klimenko <klimenko.sg@gmail.com>
2020-07-09 12:11:27 +0300

    refactor(drag-and-drop): refactor component inputs, tests and stories

--- projects/elonkit/src/ui/drag-and-drop/__stories__/drag-and-drop-story-required/drag-and-drop-story-required.source.ts
+++ projects/elonkit/src/ui/drag-and-drop/__stories__/drag-and-drop-story-required/drag-and-drop-story-required.source.ts
@@ -13,15 +13,14 @@ export const DRAG_AND_DROP_STORY_REQUIRED_SOURCE = {
     <es-drag-and-drop
       title="CHOOSE FILES"
       description="or drag files in this area (max size: 50 MB)"
-      maxSize="50"
       formControlName="docs"
-      accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,
-      application/pdf, image/jpg,image/jpeg,image/png"
-      type="binary"
+      [options]="{
+        type: 'binary'
+      }"
     >
       <mat-hint>This is an example of a hint message</mat-hint>
-      <mat-error>Select at least one file</mat-error></es-drag-and-drop
-    >
+      <mat-error>Select at least one file</mat-error>
+    </es-drag-and-drop>
     <button class="form__submit" color="primary" mat-raised-button type="submit">Submit</button>
   </form>
   `,
