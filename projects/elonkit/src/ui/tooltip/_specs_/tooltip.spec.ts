8b1f90340f87e4da6b9e724e32b37361d6af7c85
Denis Yakshov <feerzlay@gmail.com>
2020-08-10 10:22:17 +0300

    test(tooltip): add basic tests

---
+++ projects/elonkit/src/ui/tooltip/_specs_/tooltip.spec.ts
@@ -0,0 +1,91 @@
+import { Component, Input } from '@angular/core';
+import { fakeAsync, tick, flush, inject } from '@angular/core/testing';
+import { OverlayContainer } from '@angular/cdk/overlay';
+
+import { render, getByText, queryByText, RenderResult } from '@testing-library/angular';
+
+import { ESTooltipModule } from '..';
+
+const BUTTON_TEXT = 'Button';
+const BUTTON_ACTION_TEXT = 'Action';
+const BUTTON_CLOSE_TEXT = 'Close';
+const TOOLTIP_TEXT = 'Message';
+
+@Component({
+  template: `
+    <button
+      esTooltip
+      [esTooltipContent]="content"
+      [esTooltipArrow]="esTooltipArrow"
+      [esTooltipDisableFocusListener]="esTooltipDisableFocusListener"
+      [esTooltipDisableHoverListener]="esTooltipDisableHoverListener"
+      [esTooltipDisableCloseFocusListener]="esTooltipDisableCloseFocusListener"
+      [esTooltipDisableCloseHoverListener]="esTooltipDisableCloseHoverListener"
+      [esTooltipDisableCloseClickListener]="esTooltipDisableCloseClickListener"
+    >
+      ${BUTTON_TEXT}
+    </button>
+    <ng-template #content let-hide="hide">
+      ${TOOLTIP_TEXT}
+      <button>${BUTTON_ACTION_TEXT}</button>
+      <button (click)="hide()">${BUTTON_CLOSE_TEXT}</button>
+    </ng-template>
+  `
+})
+class TooltipWrapperComponent {
+  @Input() esTooltipArrow: boolean;
+  @Input() esTooltipDisableFocusListener: boolean;
+  @Input() esTooltipDisableHoverListener: boolean;
+  @Input() esTooltipDisableCloseFocusListener: boolean;
+  @Input() esTooltipDisableCloseHoverListener: boolean;
+  @Input() esTooltipDisableCloseClickListener: boolean;
+}
+
+describe('Tooltip', () => {
+  let overlay: OverlayContainer;
+  let overlayElement: HTMLElement;
+  let component: RenderResult<TooltipWrapperComponent, TooltipWrapperComponent>;
+
+  beforeEach(async () => {
+    component = await render(TooltipWrapperComponent, {
+      imports: [ESTooltipModule],
+      componentProperties: {
+        esTooltipArrow: true
+      }
+    });
+
+    inject([OverlayContainer], (oc: OverlayContainer) => {
+      overlay = oc;
+      overlayElement = oc.getContainerElement();
+    })();
+  });
+
+  afterEach(inject([OverlayContainer], (currentOverlay: OverlayContainer) => {
+    currentOverlay.ngOnDestroy();
+    overlay.ngOnDestroy();
+  }));
+
+  it('Should display a template', async () => {
+    component.fixture.componentInstance.esTooltipArrow = true;
+
+    component.mouseEnter(component.getByText(BUTTON_TEXT));
+    expect(queryByText(overlayElement, TOOLTIP_TEXT)).toBeInTheDocument();
+  });
+
+  it('Should render an arrow', async () => {
+    component.fixture.componentInstance.esTooltipArrow = true;
+
+    component.mouseEnter(component.getByText(BUTTON_TEXT));
+    expect(overlayElement.querySelector('.es-tooltip__arrow')).toBeInTheDocument();
+  });
+
+  // it('Should close tooltip by a close button click', fakeAsync(async () => {
+  //   component.fixture.componentInstance.esTooltipArrow = true;
+
+  //   component.mouseEnter(component.getByText(BUTTON_TEXT));
+  //   expect(queryByText(overlayElement, TOOLTIP_TEXT)).toBeInTheDocument();
+
+  //   component.click(getByText(overlayElement, BUTTON_CLOSE_TEXT));
+  //   expect(queryByText(overlayElement, TOOLTIP_TEXT)).not.toBeInTheDocument();
+  // }));
+});
