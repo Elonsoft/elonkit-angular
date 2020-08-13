3ea6518e1a7b4351d5891c7791caa3d45261bfe4
Denis Yakshov <feerzlay@gmail.com>
2020-08-13 14:14:47 +0300

    docs: add source code dedent

--- projects/elonkit/storybook/components/canvas/canvas.tsx
+++ projects/elonkit/storybook/components/canvas/canvas.tsx
@@ -8,6 +8,8 @@ import { Source } from '@storybook/addon-docs/blocks';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 
+import dedent from 'ts-dedent';
+
 export function Canvas({ children, source }: ICanvasProps) {
   const [value, setValue] = React.useState(source.ts ? 'ts' : source.html ? 'html' : 'scss');
   const [isSourceVisible, setSourceVisible] = React.useState(false);
@@ -46,9 +48,13 @@ export function Canvas({ children, source }: ICanvasProps) {
             {!!source.html && <Tab value='html' label='HTML' />}
             {!!source.scss && <Tab value='scss' label='SCSS' />}
           </Tabs>
-          {!!source.ts && value === 'ts' && <Source dark language='ts' code={source.ts} />}
-          {!!source.html && value === 'html' && <Source dark language='html' code={source.html} />}
-          {!!source.scss && value === 'scss' && <Source dark language='scss' code={source.scss} />}
+          {!!source.ts && value === 'ts' && <Source dark language='ts' code={dedent(source.ts)} />}
+          {!!source.html && value === 'html' && (
+            <Source dark language='html' code={dedent(source.html)} />
+          )}
+          {!!source.scss && value === 'scss' && (
+            <Source dark language='scss' code={dedent(source.scss)} />
+          )}
         </div>
       )}
     </div>
