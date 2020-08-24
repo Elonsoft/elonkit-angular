2ab871bf0a609388186062e990ab2392e3429fd5
Denis Yakshov <feerzlay@gmail.com>
2020-08-24 16:45:07 +0300

    docs: add simple documentation for colors and typography

--- projects/elonkit/src/theming/__stories__/typography/typography.component.tsx
+++ projects/elonkit/src/theming/__stories__/typography/typography.component.tsx
@@ -2,24 +2,153 @@ import * as React from 'react';
 
 export const Typography: React.FC = () => {
   return (
-    <div>
-      <h1 className='es-h1'>Heading 1</h1>
-      <h2 className='es-h2'>Heading 2</h2>
-      <h3 className='es-h3'>Heading 3</h3>
-      <h4 className='es-h4'>Heading 4</h4>
-      <h5 className='es-h5'>Heading 5</h5>
-      <h6 className='es-h6'>Heading 6</h6>
-      <div className='es-subtitle-1'>Subtitle 1</div>
-      <div className='es-subtitle-2'>Subtitle 2</div>
-      <div className='es-body-xl'>Body XL</div>
-      <div className='es-body-l'>Body L</div>
-      <div className='es-body-m'>Body M</div>
-      <div className='es-body-s'>Body S</div>
-      <div className='es-button'>BUTTON</div>
-      <div className='es-caption'>Caption</div>
-      <div className='es-overline'>OVERLINE</div>
-      <div className='es-mini-m'>Mini M</div>
-      <div className='es-mini-s'>MIni S</div>
+    <div className='es-typography-story es-body-s'>
+      <table>
+        <thead>
+          <tr>
+            <th style={{ width: '1px' }}>Class</th>
+            <th>Display</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <code>es-h1</code>
+            </td>
+            <td>
+              <div className='es-h1'>Heading 1</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-h2</code>
+            </td>
+            <td>
+              <div className='es-h2'>Heading 2</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-h3</code>
+            </td>
+            <td>
+              <div className='es-h3'>Heading 3</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-h4</code>
+            </td>
+            <td>
+              <div className='es-h4'>Heading 4</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-h5</code>
+            </td>
+            <td>
+              <div className='es-h5'>Heading 5</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-h6</code>
+            </td>
+            <td>
+              <div className='es-h6'>Heading 6</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-subtitle-1</code>
+            </td>
+            <td>
+              <div className='es-subtitle-1'>Subtitle 1</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-subtitle-2</code>
+            </td>
+            <td>
+              <div className='es-subtitle-2'>Subtitle 2</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-body-xl</code>
+            </td>
+            <td>
+              <div className='es-body-xl'>Body XL</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-body-l</code>
+            </td>
+            <td>
+              <div className='es-body-l'>Body L</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-body-m</code>
+            </td>
+            <td>
+              <div className='es-body-m'>Body M</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-body-s</code>
+            </td>
+            <td>
+              <div className='es-body-s'>Body S</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-button</code>
+            </td>
+            <td>
+              <div className='es-button'>BUTTON</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-caption</code>
+            </td>
+            <td>
+              <div className='es-caption'>Caption</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-overline</code>
+            </td>
+            <td>
+              <div className='es-overline'>OVERLINE</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-mini-m</code>
+            </td>
+            <td>
+              <div className='es-mini-m'>Mini M</div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <code>es-mini-s</code>
+            </td>
+            <td>
+              <div className='es-mini-s'>MIni S</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
