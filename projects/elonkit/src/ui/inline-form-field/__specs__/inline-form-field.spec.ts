1a561937ace2477e20f2ec29c2bd50268e92473d
Denis Yakshov <feerzlay@gmail.com>
2020-08-31 14:11:13 +0300

    test: fix typography tests

--- projects/elonkit/src/ui/inline-form-field/__specs__/inline-form-field.spec.ts
+++ projects/elonkit/src/ui/inline-form-field/__specs__/inline-form-field.spec.ts
@@ -202,9 +202,9 @@ describe('InlineFormField', () => {
       excludeComponentDeclaration: true
     });
 
-    expect(component.getByTestId('root')).toHaveClass('mat-body-1');
+    expect(component.getByTestId('root')).toHaveClass('es-body-m');
 
-    const typography = 'mat-body-2';
+    const typography = 'es-body-s';
 
     component.fixture.componentInstance.typography = typography;
     component.fixture.componentInstance.changeDetector.detectChanges();
