8253dd9812140f8889ded19347e45b79f3dde27a
Sergei Klimenko <klimenko.sg@gmail.com>
2020-09-09 18:50:44 +0300

    feat(avatar): add avatar component

---
+++ projects/elonkit/src/ui/avatar/__stories__/avatar-story-custom/index.ts
@@ -0,0 +1,3 @@
+export { AvatarStoryCustomModule } from './avatar-story-custom.module';
+export { AvatarStoryCustomComponent } from './avatar-story-custom.component';
+export { AVATAR_STORY_CUSTOM_SOURCE } from './avatar-story-custom.source';
