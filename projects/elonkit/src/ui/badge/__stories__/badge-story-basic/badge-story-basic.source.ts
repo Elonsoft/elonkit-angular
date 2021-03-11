de4a16d3dcbd733000e3c915fa20c52723f0e84a
Andrew <Fidel.Marti@yandex.ru>
2021-03-11 12:08:26 +0300

    fix(badge): add mask to badge

--- projects/elonkit/src/ui/badge/__stories__/badge-story-basic/badge-story-basic.source.ts
+++ projects/elonkit/src/ui/badge/__stories__/badge-story-basic/badge-story-basic.source.ts
@@ -1,9 +1,9 @@
 export const BADGE_STORY_BASIC_SOURCE = {
   html: `
-  <es-badge [size]="size" [color]="color" [borderColor]="borderColor">
+  <es-badge [size]="size" [color]="color" [borderColor]="borderColor" [borderSize]="borderSize">
     <es-avatar></es-avatar>
   </es-badge>
-  <es-badge [size]="size" [color]="color" [borderColor]="borderColor">
+  <es-badge [size]="size" [color]="color" [borderColor]="borderColor" [borderSize]="borderSize">
     <button type="button" color="primary" mat-icon-button>
       <mat-icon>favorite</mat-icon>
     </button>
--- projects/elonkit/src/ui/badge/__stories__/badge-story-icon/badge-story-icon.source.ts
+++ projects/elonkit/src/ui/badge/__stories__/badge-story-icon/badge-story-icon.source.ts
@@ -1,9 +1,9 @@
 export const BADGE_STORY_ICON_SOURCE = {
   html: `
-  <es-badge [size]="size" [src]="src" [alt]="alt" [borderColor]="borderColor">
+  <es-badge [size]="size" [src]="src" [alt]="alt" [borderColor]="borderColor" [borderSize]="borderSize">
     <es-avatar></es-avatar>
   </es-badge>
-  <es-badge [size]="size" [src]="src" [alt]="alt" [borderColor]="borderColor">
+  <es-badge [size]="size" [src]="src" [alt]="alt" [borderColor]="borderColor" [borderSize]="borderSize">
     <button type="button" color="primary" mat-icon-button>
       <mat-icon>favorite</mat-icon>
     </button>
