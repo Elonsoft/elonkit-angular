da7d49849c478c12c2d5a100dfb6c87ccd8a2a36
Denis Yakshov <feerzlay@gmail.com>
2021-05-13 11:03:57 +0300

    chore: bump version to 0.1.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']
+      ['chore', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']
     ]
   }
 };
